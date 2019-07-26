6a8c6686c2af666b127b70a9b91d10ef5fac5f7e
Richard Pitts <richardepitts@gmail.com>
2019-07-26 19:49:16 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -54,18 +54,18 @@ end
 
 list(songs)
 
-# def play(songs)
-#   puts "Please enter a song name or number:"
-#   user_response = gets.chomp 
+def play(songs)
+  puts "Please enter a song name or number:"
+  user_response = gets.chomp 
   
-#   if (1..9).to_a.include?(user_response.to_i)
-#     puts "Playing #{songs[user_response.to_i - 1]}"
-#     elsif songs.include?(user_response)
-#     puts "Playing #{user_response}"
-#   else 
-#     puts "Invalid input, please try again"
-#   end 
-# end 
+  if (1..9).to_a.include?(user_response.to_i)
+    puts "Playing #{songs[user_response.to_i - 1]}"
+    elsif songs.include?(user_response)
+    puts "Playing #{user_response}"
+  else 
+    puts "Invalid input, please try again"
+  end 
+end 
 
 # play(songs)
 
