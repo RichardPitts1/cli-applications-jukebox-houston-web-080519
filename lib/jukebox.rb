7c0e83d4c912bdba179e638c1ad091bda96984d7
Richard Pitts <richardepitts@gmail.com>
2019-07-26 19:48:25 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -47,53 +47,53 @@ end
 
 help 
 
-def list(songs) 
-  songs.each_with_index { |item, index|
-    puts "#{index+1}. #{item}" }
-end 
+# def list(songs) 
+#   songs.each_with_index { |item, index|
+#     puts "#{index+1}. #{item}" }
+# end 
 
-list(songs)
+# list(songs)
 
-def play(songs)
-  puts "Please enter a song name or number:"
-  user_response = gets.chomp 
+# def play(songs)
+#   puts "Please enter a song name or number:"
+#   user_response = gets.chomp 
   
-  if (1..9).to_a.include?(user_response.to_i)
-    puts "Playing #{songs[user_response.to_i - 1]}"
-    elsif songs.include?(user_response)
-    puts "Playing #{user_response}"
-  else 
-    puts "Invalid input, please try again"
-  end 
-end 
+#   if (1..9).to_a.include?(user_response.to_i)
+#     puts "Playing #{songs[user_response.to_i - 1]}"
+#     elsif songs.include?(user_response)
+#     puts "Playing #{user_response}"
+#   else 
+#     puts "Invalid input, please try again"
+#   end 
+# end 
 
-play(songs)
+# play(songs)
 
-def exit_jukebox
-  puts "Goodbye"
-end 
+# def exit_jukebox
+#   puts "Goodbye"
+# end 
 
-def run(songs)
-  #help
-  command = "" 
-  while command  
-  puts "Please enter a command:"
-  command = gets.downcase.strip  
-  case command 
-    when 'list'
-      list(songs)
-      when 'play'
-        list(songs)
-        play(songs)
-      when 'help'
-        help 
-      when 'exit'
-        exit_jukebox
-        break 
-      else 
-        help 
-      end 
-    end 
-  end 
-  run(songs)
+# def run(songs)
+#   #help
+#   command = "" 
+#   while command  
+#   puts "Please enter a command:"
+#   command = gets.downcase.strip  
+#   case command 
+#     when 'list'
+#       list(songs)
+#       when 'play'
+#         list(songs)
+#         play(songs)
+#       when 'help'
+#         help 
+#       when 'exit'
+#         exit_jukebox
+#         break 
+#       else 
+#         help 
+#       end 
+#     end 
+#   end 
+#   run(songs)
     
